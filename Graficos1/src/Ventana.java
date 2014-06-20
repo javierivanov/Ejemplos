09d2c947797b8d87ba1ce154d3852833b0776778
Javier Fuentes <0xffaa.rm@gmail.com>
2014-06-20 02:54:03 -0400

    create project

--- Graficos1/src/Ventana.java
+++ Graficos1/src/Ventana.java
@@ -11,6 +11,7 @@ public class Ventana extends JFrame{
     {
         super("Titulo");
         setSize(640, 480);
+        setDefaultCloseOperation(EXIT_ON_CLOSE); //permite terminar la appp cuando la ventana se cierre
         setLocationRelativeTo(null); //Ubica la ventana en el centro de la pantalla
         panel = new Panel();
         add(panel);
